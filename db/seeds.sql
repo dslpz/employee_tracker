852c1c4fae3c2c1f14430e8432c4eecf25c56760
Damaris Palacios <dslpz728@gmail.com>
2022-06-07 21:19:46 -0500

    adds to seeds

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (first_name, last_name, id)
+
+
+INSERT INTO department (id, name)
+
+
+INSERT INTO role (id, title, salary, department_id)
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-    
+ const express = require("express");
+ const db = require("./db/connection");
+ 
+ const PORT = process.env.PORT || 3006;
+ const app = express();
+
+ var connection = mysql.createConnection({
+     host: "localhost",
+     port: 3306,
+     database: "employee_trackerr",
+ });
